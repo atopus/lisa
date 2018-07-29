4f023125c3ef6e1cab56db21378815317393ae46
Paul Antropius <paul.antropius@kstor.net>
2018-07-29 12:08:16 +0200

    Fixtures revision.

--- fixtures/data.js
+++ fixtures/data.js
@@ -53,93 +53,90 @@ const DATA = {
     scale : {
       1 : "La... ? méthode ? C'est quoi ???",
       2 : "C'est ça la méthode ? Bof. Je préfère foncer direct, c'est mieux !",
-      3 : "D'accord, la méthode, c'est bien, mais seulement quand je bloque. Sinon, c'est trop genre une perte de temps",
-      4 : "Mouais... peut-être que la méthode n'est pas une perte de temps. En l'utilisant tous les jours, c'est pourrait aller plus vite",
-      5 : "Oui, c'est vrai que sans méthode, ça devient vite compliqué.",
-      6 : "T'as pas de méthode ? Non mais allô quoi !",
+      3 : "D'accord, la méthode, c'est bien, mais seulement quand je bloque. Sinon, c'est trop genre une perte de temps.",
+      4 : "Mouais... C'est peut-être utile. Faut voir...",
+      5 : "Mince, c'est vrai que c'est plus simple avec un peu de méthode !",
+      6 : "T'as pas de méthode ?? Non mais allô quoi !",
       7 : "J'ai préparé mon travail à l'avance, et ai appliqué la méthode que je connais",
-      8 : "Je réajuste ma méthode en fonction de mes résultats, de mes contraintes et de mes besoins",
+      8 : "Je réajuste ma méthode en fonction de mes résultats, de mes contraintes et de mes besoins. Trop fort !",
     },
     thresholds : [4, 5, 7]
   },
-  // [MOTIVATION] : {
-  //   label : MOTIVATION,
-  //   scale: {
-  //     1 : "C'est nul, ça sert à rien !!",
-  //     2 : ""
-  //   }
-  // },
   [ENDURANCE]  :{
     label: "Endurance / concentration",
     min : 0,
     max: 8,
-    unit: 'heure(s)'
+    unit: 'heure(s)',
+    thresholds: [1, 3, 4]
   },
   [CONSCIOUSNESS] : {
-    label: "Conscience",
+    label: "Conscience de soi",
     scale: {
-      1 : "J'invente des excuses",
-      2 : "Je préfère ne pas voir, ne pas savoir.",
-      5 : "Je ne me juge pas",
+      1 : "Les profs, c'est tous des NULS !",
+      2 : "J'veux pas savoir !",
+      3 : "J'y arriverai jamais !!",
+      4 : "Ah, c'est pas complètement impossible, en fin de compte. Mais c'est chaud !",
+      5 : "Plutôt que de me mettre martel en tête, au boulot. On verra bien.",
       6 : "J'ai conscience de mes limites",
-      7 : "J'ai connais ma capacité de travail",
-      8 : "Je sais identifier mes différents états mentaux",
+      7 : "Ayé, je connais ma capacité de travail.",
+      8 : "Mieux encore : je sais identifier mes différents états mentaux : panique, stress, énervée, concentrée.",
       9 : "Je respecte mes états mentaux tout en étant capable de me rediriger"
-    }
+    },
+    thresholds: [3, 5, 7]
   },
-  // [PREPARATION] : {
-  //   label: PREPARATION
-  // },
   [MATHS] : {
     label: "Mathématiques",
     scale : {
       1 : "Plus JAMAIS !",
-      2 : "J'y pige rien",
+      2 : "C'est simple : j'y pige rien.",
       3 : "Pourquoi, au juste ?",
-      4 : "Ok, je crois que j'ai compris",
-      5 : "En fait, c'est pas encore au point",
-      7 : "Les exercices sont à peu près corrects, à peu près dans les temps ",
-      8 : "Les exercices sont bons, dans les temps",
-      9 : "Je suis en avance, tout le temps de me relire",
-      10 : "Top !"
-    }
+      4 : "Ok, je crois que j'ai compris. Mais j'ai pas franchement envie de vérifier.",
+      5 : "Pigé en théorie, mais dans la pratique, c'est pas encore au point. Trop de temps, trop d'erreurs...",
+      6 : "Les exercices sont grosso modo corrects, grosso modo dans les temps.",
+      7 : "Les exercices sont bons, dans les temps",
+      8 : "Je suis en avance, tout le temps de me relire",
+      9 : "Top !"
+    },
+    thresholds : [4, 6, 7]
   },
   [FRANCAIS] : {
     label: "Français",
     scale : {
       1 : "c tro dur",
+      2 : "Plus d'une erreur d'orthographe pour 30 mots",
       3 : "Moins d'une erreur d'orthographe pour 30 mots",
-      4 : "Moins d'une erreur d'orthographe pour 30 mots",
-      5 : "Moins d'une erreur d'orthographe pour 30 mots",
-      6 : "Moins d'une erreur d'orthographe pour 30 mots",
-      7 : "Moins d'une erreur d'orthographe pour 60 mots",
-      8 : "Quasi aucune erreur. La rédaction, les styles, je suis au point",
-      9 : "J'écris pour le plaisir",
-      10 : "Je viens de terminer Les Rougons-Maquards. Une petite fiche critique s'impose."
-    }
+      4 : "Moins d'une erreur d'orthographe pour 60 mots",
+      5 : "Quasi aucune erreur. La rédaction, les styles, je suis au point",
+      6 : "J'écris pour le plaisir",
+      7 : "Je viens de terminer Les Rougons-Maquards. Une petite fiche critique s'impose."
+    },
+    thresholds : [3, 4, 5]
   },
   [HISTOIRE] : {
     label : "Histoire",
     scale: {
       1 : "Je connais trop bien la guerre de 78 !",
-      2 : "Je sais répéter mon cours d'histoire",
-      3 : "Je connais mon cours d'histoire",
-      4 : "Je connais l'Histoire",
-      9 : "Où que mon regard se promène, j'y reconnais les traces de notre civilisation et les marques de notre passé. Diantre ! Quelles merveilles !",
+      2 : "Je connais à peu près les dates obligatoires. Faudrait pas m'en demander plus.",
+      3 : "Je sais répéter quelques morceaux de mon cours d'histoire, si on me pose des questions précises.",
+      4 : "Je connais mon cours d'histoire. J'arrive à tisser quelques liens entre les leçons, mais c'est encore limité.",
+      5 : "Ok, je maîtrise à correctement mon cours d'histoire : j'arrive sans trop de fatigue à associer mes connaissances en boucle.",
+      6 : "Non, je ne connais pas mon cours d'histoire. Je connais l'Histoire.",
+      7 : "Où que mon regard se promène, j'y reconnais les traces de notre civilisation et les marques de notre passé. Diantre ! Quelles merveilles !",
       10 : "Mes cours sont lacunaires. Je vais les compléter."
-    }
+    },
+    thresholds : [3, 5, 6]
   },
   [ANGLAIS] : {
     label : "Anglais",
     scale : {
-      1 : "Heu...",
+      1 : "Heu... quoi ?",
       2 : "Ich liebe dich ?",
       3 : "Mai telor is riche",
-      4 : "My taylor is rich",
+      4 : "My taylor is rich.",
       5 : "All right, I know the very basics of english : verbs, sentence construction, pronouns, and maybe a few hundred words",
-      6 : "I may also know the main tenses : past, present, future. Even conditional, and every irregular verbs. I'm able to read and undestand a simple text",
+      6 : "I may also know the main tenses : past, present, future. Even conditional, and every irregular verbs. I'm able to read and understand a simple text",
       7 : "I'm fluent enough to order pancakes in a restaurant.",
-      8 : "I undestand and sing my favorite songs",
+      8 : "I understand and sing my favorite songs",
       9 : "I really don't need to be evaluated any more. It's natural.",
       10 : "Look, I do think we should practice our english on a regular basis. Let's start right now !"
     }
